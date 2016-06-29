78a156c4463e0a87544d1387e7a793ad0fd82061
Jonathan Prieto <prieto.jona@gmail.com>
2016-06-28 21:37:02 -0500

    added comments in the source code to fullfil with the requirement of the homework

--- lab3/FOL.hs
+++ lab3/FOL.hs
@@ -1,7 +1,7 @@
 -- @Author: Jonathan Prieto
 -- @Date:   2016-06-28 17:18:00
 -- @Last Modified by:   Jonathan Prieto
--- @Last Modified time: 2016-06-28 19:13:27
+-- @Last Modified time: 2016-06-28 21:31:58
 module FOL where
 
 type Ft = String
--- lab3/Unify.hs
+++ lab3/Unify.hs
@@ -1,12 +1,15 @@
 -- @Author: Jonathan Prieto
 -- @Date:   2016-06-28 11:10:10
 -- @Last Modified by:   Jonathan Prieto
--- @Last Modified time: 2016-06-28 19:10:55
+-- @Last Modified time: 2016-06-28 21:36:21
 module Unify
     where
 
 import           FOL
 
+-- The type of error for unify' and unify functions.
+-- When two atoms are not unifiable, they returns one
+-- of these errors.
 data UnifyError =
     DifferentPred
     | DifferentPredArity
@@ -15,6 +18,10 @@ data UnifyError =
     | FailRule4
     deriving (Eq, Read, Show)
 
+-- The function unify is the implementation of Martelli-Montanari
+-- algorithm. First, it evaluates the conditions over the predicates,
+-- and second, if they pass, it calls the unify' function to do the rest of the
+-- algorithm. If something was wrong, it returns an error of UnifyError type.
 unify :: Atom -> Atom -> Either UnifyError [Atom]
 unify p@(Pred f xs) q@(Pred g ys)
     | f /= g                    = Left DifferentPred
@@ -25,6 +32,11 @@ unify p@(Pred f xs) q@(Pred g ys)
                 Left x    -> Left x
                 Right mgu -> Right [replaceWith p mgu, replaceWith q mgu]
 
+-- The function unify' applies the steps of the Martelli-Montanari algorithm
+-- a set of rules. When it fails to find out the most general unifies,
+-- it returns one of the errors of UnifyError type.
+-- When it succeded, it returns a substituion, given by
+-- a list of pairs of terms (variable <- term).
 unify' :: [(Term, Term)] -> Either UnifyError [(Term, Term)]
 unify' ((F f ts, Var x):xs)     = unify' $ xs ++ [(Var x, F f ts)]
 unify' ((F f xs, F g ys):ws)
@@ -51,26 +63,36 @@ unify' ((Var x, F f ts):xs)
                     Right mgu -> Right $ (Var x, F f ts) : mgu
 unify' [] = Right []
 
+-- The function vars returns all vars from a term
 vars :: Term -> [Term]
 vars (Var x)    = [Var x]
 vars (F _ xs)   = vars' xs
 
+-- The function vars' returns all vars from a list of terms 
 vars' :: [Term] -> [Term]
 vars' (Var x : xs)  = Var x : vars' xs
 vars' (F _ ts : xs) = vars' ts ++ vars' xs
 vars' []            = []
 
+-- The function vars'' returns all vars from a set of equations
+-- given by a list of pairs of terms
 vars'' :: [(Term, Term)]-> [Term]
 vars'' = vars' . uncurry (++) . unzip
 
+-- The function sub replaces a term x by y in all occurrencies
+-- in a term
 sub :: Term -> Term -> Term -> Term
 sub (Var x) y (Var z)   = if x == z then y else Var z
 sub (Var x) y (F f ts)  = F f (map (sub (Var x) y) ts)
 sub _ _ t               = t
 
+-- The function sub' replaces a term x by y in all occurrencies
+-- in a pair of terms 
 sub' :: Term -> Term -> (Term, Term) -> (Term, Term)
 sub' x y (t1, t2) = (sub x y t1, sub x y t2)
 
+-- The function replaceWith applies a substitution to an atom.
+-- The substituion is given by a list of pairs of terms.
 replaceWith :: Atom -> [(Term, Term)] -> Atom
 replaceWith (Pred p ts) ((x, t):xs) = newts
     where
